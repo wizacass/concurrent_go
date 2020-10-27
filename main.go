d047cc2e743d1aef186d893d3dc49f198bb2efe2
Vytenis Petrauskas <vytenis.p@oxylabs.io>
2020-10-27 17:15:28 +0200

    Implement logic to calculate computed car value

--- main.go
+++ main.go
@@ -4,14 +4,20 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"math"
 	"os"
 )
 
 type car struct {
 	Model          string
-	Price          float32
+	Price          float64
 	Period         int
-	InitialPayment float32
+	InitialPayment float64
+}
+
+type computedCar struct {
+	car           car
+	computedValue float64
 }
 
 func check(err error) {
@@ -35,11 +41,35 @@ func read(filename string) []car {
 	return cars
 }
 
+func getComputedValue(c car) float64 {
+	contractPrice := 300
+	interestRate := 0.03
+	realPrice := c.Price - c.InitialPayment
+	fullPrice := realPrice*math.Pow((1+interestRate), float64(c.Period)) + float64(contractPrice)
+	monthlyPayment := fullPrice / float64(c.Period)
+
+	return math.Round(monthlyPayment*100) / 100
+}
+
+func getComputedCar(c car) computedCar {
+	value := getComputedValue(c)
+	compCar := computedCar{c, value}
+	return compCar
+}
+
 func main() {
 	filename := "data/IFF8-1_PetrauskasV_L1_dat_1.json"
-
-	fmt.Println("Hello, world!")
+	var threshold float64 = 5000
 
 	var cars = read(filename)
 	fmt.Println("Total cars: ", len(cars))
+	for i := 0; i < len(cars); i++ {
+		cc := getComputedCar(cars[i])
+		ok := ""
+		if cc.computedValue < threshold {
+			ok = "OK"
+		}
+		outString := fmt.Sprintf("%6s %9.2f ", cc.car.Model, cc.computedValue)
+		fmt.Println(outString, ok)
+	}
 }
